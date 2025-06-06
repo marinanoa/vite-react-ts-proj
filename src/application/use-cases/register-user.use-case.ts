5490e3954711206ae0aad169371d7f6623140a3a
Marina <marina.lopez@king.com>
2025-06-06 11:54:02 +0200

    refactor: :recycle: Add RegisterRequestDto.

--- src/application/use-cases/register-user.use-case.ts
+++ src/application/use-cases/register-user.use-case.ts
@@ -1,7 +1,8 @@
 import type { AuthRepository } from '../../domain/repositories/AuthRepository';
+import type { RegisterRequestDto } from '../dtos/RegisterRequestDto';
 
 export const registerUser = (repo: AuthRepository) => {
-  return async (input: { name: string; email: string; password: string }) => {
-    return await repo.register(input);
+  return async (registerRequest: RegisterRequestDto) => {
+    return await repo.register(registerRequest);
   };
 };
